d195a7591f80d1e7b102b7c0c0198c0a5fe6cf85
Kozcoko <116641538+Kozcoko@users.noreply.github.com>
2024-02-17 18:30:31 -0500

    Finish in a day without touching code for 3 months.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,3 @@
-let naime = prompt("Well 'ello there! Whaht's your naime?", "Name here, please!");
+//let naime = prompt("Well 'ello there! Whaht's your naime?", "Name here, please!");
 
-if (naime === "Steve" || "steve") {
-    open(href="https://www.youtube.com", target="blank", rel="noopener, noreferrer")
-}
-else alert("Oh, you're not who I'm looking for.");
-window.close;
+//alert("Oh, you're not who I'm looking for.");
