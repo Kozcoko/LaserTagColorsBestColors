d3e2bd8f294f3e4bcd3602144c95bf7820e6a47e
Kozcoko <116641538+Kozcoko@users.noreply.github.com>
2024-02-17 14:21:13 -0500

    Fiddle with open()

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let naime = prompt("Well 'ello there! Whaht's your naime?", "Name here, please!");
+
+if (naime === "Steve" || "steve") {
+    open(href="https://www.youtube.com", target="blank", rel="noopener, noreferrer")
+}
+else alert("Oh, you're not who I'm looking for.");
+window.close;
